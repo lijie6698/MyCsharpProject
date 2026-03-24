cefc2544002084147c2638960fa45405e01e1900
lijie6698 <lj2gjy@163.com>
2026-03-24 09:27:00 +0800

    Add designer files for WinForms and update project file to include all new forms and controls

---
+++ LoginForm.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace DeepTrainingTest
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(120, 30);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(150, 21);
+            this.txtUsername.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(120, 70);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(150, 21);
+            this.txtPassword.TabIndex = 1;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(50, 33);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(41, 12);
+            this.lblUsername.TabIndex = 2;
+            this.lblUsername.Text = "用户名:";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(50, 73);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(41, 12);
+            this.lblPassword.TabIndex = 3;
+            this.lblPassword.Text = "密码:";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(50, 110);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "登录";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(150, 110);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "取消";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 151);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "用户登录";
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -4,7 +4,7 @@
     {
         /// <summary>
         /// 必需的设计器变量。
-        /// </summary>
+        /// </private
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
@@ -25,7 +25,7 @@
         /// <summary>
         /// 设计器支持所需的方法 - 不要修改
         /// 使用代码编辑器修改此方法的内容。
-        /// </>
+        /// </summary>
         private void InitializeComponent()
         {
             this.panelMain = new System.Windows.Forms.Panel();
@@ -33,33 +33,78 @@
             this.btnDatabase = new System.Windows.Forms.Button();
             this.btnPLC = new System.Windows.Forms.Button();
             this.btnVision = new System.Windows.Forms.Button();
-            this.panelMain.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelMain
             // 
             this.panelMain.Location = new System.Drawing.Point(150, 12);
             this.panelMain.Name = "panelMain";
-            this.panelMain.Size = new System.Drawingibo mainForm = new MainForm();
-mainForm.Show();
-this.Hide();
-            }
-            else
-            {
-                MessageBox.Show("用户名或密码错误！", "登录失败", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                txtPassword.Clear();
-                txtPassword.Focus();
-            }
+            this.panelMain.Size = new System.Drawing.Size(800, 600);
+            this.panelMain.TabIndex = 0;
+            // 
+            // btnLog
+            // 
+            this.btnLog.Location = new System.Drawing.Point(12, 200);
+            this.btnLog.Name = "btnLog";
+            this.btnLog.Size = new System.Drawing.Size(120, 40);
+            this.btnLog.TabIndex = 4;
+            this.btnLog.Text = "日志记录";
+            this.btnLog.UseVisualStyleBackColor = true;
+            this.btnLog.Click += new System.EventHandler(this.btnLog_Click);
+            // 
+            // btnDatabase
+            // 
+            this.btnDatabase.Location = new System.Drawing.Point(12, 150);
+            this.btnDatabase.Name = "btnDatabase";
+            this.btnDatabase.Size = new System.Drawing.Size(120, 40);
+            this.btnDatabase.TabIndex = 3;
+            this.btnDatabase.Text = "数据库";
+            this.btnDatabase.UseVisualStyleBackColor = true;
+            this.btnDatabase.Click += new System.EventHandler(this.btnDatabase_Click);
+            // 
+            // btnPLC
+            // 
+            this.btnPLC.Location = new System.Drawing.Point(12, 100);
+            this.btnPLC.Name = "btnPLC";
+            this.btnPLC.Size = new System.Drawing.Size(120, 40);
+            this.btnPLC.TabIndex = 2;
+            this.btnPLC.Text = "PLC连接";
+            this.btnPLC.UseVisualStyleBackColor = true;
+            this.btnPLC.Click += new System.EventHandler(this.btnPLC_Click);
+            // 
+            // btnVision
+            // 
+            this.btnVision.Location = new System.Drawing.Point(12, 50);
+            this.btnVision.Name = "btnVision";
+            this.btnVision.Size = new System.Drawing.Size(120, 40);
+            this.btnVision.TabIndex = 1;
+            this.btnVision.Text = "视觉检测";
+            this.btnVision.UseVisualStyleBackColor = true;
+            this.btnVision.Click += new System.EventHandler(this.btnVision_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(962, 624);
+            this.Controls.Add(this.btnLog);
+            this.Controls.Add(this.btnDatabase);
+            this.Controls.Add(this.btnPLC);
+            this.Controls.Add(this.btnVision);
+            this.Controls.Add(this.panelMain);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "主界面";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
         }
 
-        private void btnCancel_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
-        }
+        #endregion
 
-        private void LoginForm_Load(object sender, EventArgs e)
-        {
-            txtUsername.Focus();
-        }
+        private System.Windows.Forms.Panel panelMain;
+        private System.Windows.Forms.Button btnLog;
+        private System.Windows.Forms.Button btnDatabase;
+        private System.Windows.Forms.Button btnPLC;
+        private System.Windows.Forms.Button btnVision;
     }
 }
